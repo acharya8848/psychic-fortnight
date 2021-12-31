4d7eae62282ef777a6d9e71277aba11db86e1e74
ryangrayson1 <rtg5xkh@virginia.edu>
2021-12-31 16:37:07 -0700

    heroku backend changes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -6,6 +7,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+//// Have Node serve the files for our built React app (for heroku deploy)
+app.use(express.static(path.resolve(psychic-fortnight, '../frontend/build')));
 
 const PORT = process.env.PORT || 3001;
 
@@ -15,6 +19,10 @@ app.get("/", (req, res) => {
 
 app.get("/api", (req, res) => {
   res.json({ message: "*example JSON data from the backend*", name: "ryan", school: "UVA", major: "CS" });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(psychic-fortnight, '../frontend/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
