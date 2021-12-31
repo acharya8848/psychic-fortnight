720fdb5a0585a072eef5dfcd5f1a72ae6761edf6
ryangrayson1 <rtg5xkh@virginia.edu>
2021-12-31 16:06:59 -0700

    frontend and backend connected successfully with example data

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Profile() {
+    //getting data from backend... (profile data as JSON)
+    const [profileData, setProfileData] = useState(null);
+     
+    useEffect(() => {
+        fetch("/api")
+          .then((res) => res.json())
+          .then((profileData) => setProfileData(profileData));
+      }, []);
 
     return(
         <div>
-            <h2 className="words">This is your Profile page.</h2>
+            <h2 className="words">This is your Profile page.</h2><br/>
+            
+            {!profileData ? 
+                "Loading profile data..." : 
+                <>
+                <h2 className="clean dataexample">{profileData.message}</h2>
+                <table className="table clean">
+                    <tbody>
+                        <tr>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+                </>
+            }
         </div>
     )
 };
