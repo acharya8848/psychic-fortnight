83a59c0230b93bed90a180692905d8c5c7d624d7
ryangrayson1 <rtg5xkh@virginia.edu>
2021-12-31 10:03:14 -0700

    attempting to add particles effect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/App.css';
 import fire from './fire.js';
 import Login from './components/Login.jsx';
 import Profile from './components/Profile.jsx';
 import Home from './components/Home.jsx';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Particles from 'react-particles-js';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
+  //particles effect
+  const [showParticles, setShowParticles] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowParticles(true);
+    }, 500);
+  }, []);
+  //end particles effect
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
@@ -35,14 +47,19 @@ function App() {
             </>
           ) 
           : (
-            <>
-              <h1><p>Welcome!</p></h1>
+            <> 
               <span onClick={signOut}>
                 <a href="/">Sign out</a>
               </span>
+              <span>
+                <a href="/profile/">Profile</a>
+              </span>
+              <span>
+                <a href="/">Home</a>
+              </span>
+              <h1><p>Welcome!</p></h1>
               <Routes>
                 <Route path="/profile/" element={<Profile/>}>
-                  Profile
                 </Route>
                 <Route path="/" element={<Home/>}>
                 </Route>
@@ -50,6 +67,85 @@ function App() {
             </>
           )}
       </Router>
+            
+
+
+      {showParticles && (
+        <Particles
+          className="particles"
+          height="95%"
+          width="95%"
+          params={{
+            particles: {
+              number: {
+                value: 120,
+                density: { enable: true, value_area: 700 },
+              },
+              color: { value: "#8465eb" },
+              shape: {
+                type: "circle",
+                stroke: { width: 0, color: "#000000" },
+                polygon: { nb_sides: 5 },
+                image: { src: "img/github.svg", width: 100, height: 100 },
+              },
+              opacity: {
+                value: 0.5,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false,
+                },
+              },
+              size: {
+                value: 5,
+                random: true,
+                anim: { enable: false, speed: 80, size_min: 0.1, sync: false },
+              },
+              line_linked: {
+                enable: true,
+                distance: 150,
+                color: "#79fce5",
+                opacity: 0.3,
+                width: 2,
+              },
+              move: {
+                enable: true,
+                speed: 4,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: { enable: false, rotateX: 600, rotateY: 1200 },
+              },
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: { enable: true, mode: "repulse" },
+                onclick: { enable: true, mode: "bubble" },
+                resize: true,
+              },
+              modes: {
+                grab: { distance: 400, line_linked: { opacity: 1 } },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3,
+                },
+                repulse: { distance: 200, duration: 0.4 },
+                push: { particles_nb: 4 },
+                remove: { particles_nb: 2 },
+              },
+            },
+            retina_detect: true,
+          }}
+        />
+      )}
     </div>
   );
 }
--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 function Profile() {
 
     return(
         <div>
             Welcome to your Profile page.
-            <Link to="/">Home</Link>
         </div>
     )
 };
