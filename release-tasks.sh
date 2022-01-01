321650da151b0659f892fe62b8b7986768e26d0a
acharya8848 <anubhavacharya@icloud.com>
2022-01-01 15:10:34 -0500

    Heroku fix attempt 6

---
+++ release-tasks.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd /app/frontend
+npm i
+npm run build
+cd /app/backend
+npm i
--- wrapper.js
+++ wrapper.js
@@ -1,11 +1,9 @@
 var fork = require('child_process').fork;
+const exec = require('child_process').exec;
+
+console.log('Starting backend server');
 var server = fork('./backend/index.js');
 
-const exec = require('child_process').exec;
-exec('npm run build --prefix frontend', (err, stdout) => {
-  console.log(stdout);
-  console.log('over');
-});
 exec('npm start --prefix frontend', (err, stdout) => {
   console.log(stdout);
   console.log('over');
