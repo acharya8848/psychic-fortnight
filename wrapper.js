39871d4cea70c2fdb4758d3fd7c8d336e85d1c71
acharya8848 <anubhavacharya@icloud.com>
2022-01-01 14:40:18 -0500

    Heroku fix attemp 5

--- wrapper.js
+++ wrapper.js
@@ -2,7 +2,11 @@ var fork = require('child_process').fork;
 var server = fork('./backend/index.js');
 
 const exec = require('child_process').exec;
+exec('npm run build --prefix frontend', (err, stdout) => {
+  console.log(stdout);
+  console.log('over');
+});
 exec('npm start --prefix frontend', (err, stdout) => {
-          console.log(stdout);
-          console.log('over');
-        });
+  console.log(stdout);
+  console.log('over');
+});
