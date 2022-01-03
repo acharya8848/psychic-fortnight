132b30faf3c89fbe9fb7f17bad5484f9aadf6ba2
acharya8848 <anubhavacharya@icloud.com>
2022-01-03 16:27:44 -0500

    Database integration into backend

--- backend/index.js
+++ backend/index.js
@@ -30,33 +30,34 @@ async function run() {
 
   try {
 
-    connection = await oracledb.getConnection(dbConfig);;
+    connection = await oracledb.getConnection(dbConfig);
 
     // Create a table
-    await connection.execute(`begin
-                                execute immediate 'drop table nodetab';
-                                exception when others then if sqlcode <> -942 then raise; end if;
-                              end;`);
-    await connection.execute("IF (EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'TheSchema' AND  TABLE_NAME = 'TheTable'))");
+    // await connection.execute(`begin
+    //                             execute immediate 'drop table nodetab';
+    //                             exception when others then if sqlcode <> -942 then raise; end if;
+    //                           end;`);
+    let exists = connection.tableExists('nodetab');
 
-    await connection.execute(`create table nodetab (id number, data varchar2(20))`);
+    if (exists) {
+      console.log('Table exists');
+    } else {
+      await connection.execute(`create table nodetab (firstName varchar(20), lastName varchar2(20), school varchar2(20), major varchar(20))`);
 
-    // Insert some rows
-
-    const sql = `INSERT INTO nodetab VALUES (:1, :2)`;
-
-    const binds =
-      [ [1, "First" ],
-        [2, "Second" ],
-        [3, "Third" ],
-        [4, "Fourth" ],
-        [5, "Fifth" ],
-        [6, "Sixth" ],
-        [7, "Seventh" ] ];
-
-    await connection.executeMany(sql, binds);
-
-    // connection.commit();     // uncomment to make data persistent
+      // Insert some rows
+  
+      const sql = `INSERT INTO nodetab VALUES (:1, :2, :3, :4)`;
+  
+      const binds =
+        [ ["Ryan", "Grayson", "UVA", "Computer Science"],
+          ["Anubhav", "Acharya", "UVA", "Computer Engineering"],
+          ["Jhon", "Doe", "VT", "Electrical Engineering"],
+          ["Jane", "Doe", "VT", "Data Science"] ];
+  
+      await connection.executeMany(sql, binds);
+  
+      // connection.commit();     // uncomment to make data persistent
+    }
 
     // Now query the rows back
 
