042c5cf77b53e741384be452c6211367d0058dca
ryangrayson1 <rtg5xkh@virginia.edu>
2022-01-03 10:42:55 -0700

    heroku deployment attempt 9

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve('../frontend/build')));
 
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Baseline server working");
-});
-
 app.get("/api", (req, res) => {
   res.json({ message: "*example JSON data from the backend*", name: "ryan", school: "UVA", major: "CS" });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve('../frontend/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
