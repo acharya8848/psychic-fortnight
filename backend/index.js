1a39a1873d64ad9cdbd94fa01e9dfd949fe756f5
ryangrayson1 <rtg5xkh@virginia.edu>
2022-01-04 09:16:04 -0700

    mongo database connection successful using mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,111 +1,26 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
-const fs = require('fs');
-const oracledb = require('oracledb');
-const dbConfig = require('./dbconfig.js');
 const path = require('path');
-
-let libPath;
-if (process.platform === 'win32') {           // Windows
-  libPath = 'C:\\oracle\\instantclient_21_3';
-} else if (process.platform === 'darwin') {   // macOS
-  libPath = process.env.HOME + '/Downloads/instantclient_19_8';
-}
-if (libPath && fs.existsSync(libPath)) {
-  oracledb.initOracleClient({ libDir: libPath });
-}
-
 const app = express();
+
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(
+  process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+  console.log('Connected to database');
+})
+.catch((err) => {
+  console.log('Error connecting to DB', err.message);
+});
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-
-async function createTable(connection, table){
-  await connection.execute(`create table `+table);
-}
-
-async function addColumn(connection, table, columns){
-  await connection.execute(`INSERT INTO `+table+` ( `+columns+` );`);
-}
-
-// These functions are unncessary, only for SQL reference
-async function addRowsSingleColumn(connection, table, column, rows){
-  await connection.execute(`INSERT INTO `+table+` ( `+column+` ) VALUES ( `+rows+` );`);
-}
-
-async function addRowsEntireTable(connection, table, rows){
-  await connection.execute(`INSERT INTO `+table+` VALUES ( `+rows+` );`);
-}
-
-async function run() {
-
-  let connection;
-
-  try {
-
-    connection = await oracledb.getConnection(dbConfig);
-
-    // Create a table
-    // await connection.execute(`begin
-    //                             execute immediate 'drop table nodetab';
-    //                             exception when others then if sqlcode <> -942 then raise; end if;
-    //                           end;`);
-
-    // Check if the table exists
-    let exists = await connection.execute(`select table_name from user_tables where table_name = 'nodetab'`);
-
-    if (exists.rows.length != 0) {// If the table exists, print a line on the console and then move on to query data from the table
-      console.log('Table exists');
-    } else {// If not, then create a table and put some data in it and then move on to query data from the table
-      await connection.execute(`create table nodetab (firstName varchar(20), lastName varchar2(20), school varchar2(20), major varchar(30))`);
-
-      // Insert some rows
-      const sql = `INSERT INTO nodetab VALUES (:1, :2, :3, :4)`;
-  
-      const binds =
-        [ ["Ryan", "Grayson", "UVA", "Computer Science"],
-          ["Anubhav", "Acharya", "UVA", "Computer Engineering"],
-          ["Jhon", "Doe", "VT", "Electrical Engineering"],
-          ["Jane", "Doe", "VT", "Data Science"] ];
-  
-      await connection.executeMany(sql, binds);
-  
-      // connection.commit();     // uncomment to make data persistent
-    }
-
-    // Now query the rows back
-    const result = await connection.execute(`SELECT * FROM nodetab`);
-
-    return result.rows;
-
-  } catch (err) {
-    console.error(err);
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  }
-}
-
-let data = run();
-
-/* This is what the string in 'data' will look like:
-[
-  [ 'Ryan', 'Grayson', 'UVA', 'Computer Science' ],
-  [ 'Anubhav', 'Acharya', 'UVA', 'Computer Engineering' ],
-  [ 'Jhon', 'Doe', 'VT', 'Electrical Engineering' ],
-  [ 'Jane', 'Doe', 'VT', 'Data Science' ]
-]
-I don't know how to put this into the table in HTML, help!
-*/
 
 app.get("/api", (req, res) => {
   res.json({"table": data});
